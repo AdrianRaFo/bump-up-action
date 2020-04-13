e4746cc9db30a6eb5e65d7b97967990c3fb379b7
AdrianRaFo <15971742+AdrianRaFo@users.noreply.github.com>
2020-04-13 23:20:37 +0200

    delete unused

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,7 @@
 
 #Args
 set -u
-VERSION_FILE=$1
-VERSION_KEY=$2
-
-#Get version info
-VERSION=$(cat "$VERSION_FILE" | grep "$VERSION_KEY" | cut -d'=' -f2 | tr -d '[:space:]')
+VERSION=$1
 
 if [[ "$VERSION" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
   IS_RELEASE=true
@@ -13,22 +9,7 @@ else
   IS_RELEASE=false
 fi
 
-if [[ "$VERSION" == '*-SNAPSHOT' ]]; then
-  IS_SNAPSHOT=true
-else
-  IS_SNAPSHOT=false
-fi
-
-if [[ $IS_RELEASE == 'true' || $IS_SNAPSHOT == 'true' ]]; then
-  HAS_VALID_FORMAT=true
-else
-  HAS_VALID_FORMAT=false
-fi
-
-echo "::set-env name=VERSION::$VERSION"
 echo "::set-env name=IS_RELEASE::$IS_RELEASE"
-echo "::set-env name=IS_SNAPSHOT::$IS_SNAPSHOT"
-echo "::set-env name=HAS_VALID_FORMAT::$HAS_VALID_FORMAT"
 
 # Bump up
 
