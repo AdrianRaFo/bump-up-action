de9f54ad65304927c6b80bcb4c81588ff087d8d8
AdrianRaFo <15971742+AdrianRaFo@users.noreply.github.com>
2020-04-14 13:32:27 +0200

    finish and clean up action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,11 @@
 
 #Args
 set -u
-VERSION=$1
+VERSION_FILE=$1
+VERSION_KEY=$2
+VERSION_VALUE=$3
 
-if [[ "$VERSION" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+if [[ "$VERSION_VALUE" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
   IS_RELEASE=true
 else
   IS_RELEASE=false
@@ -14,13 +16,14 @@ echo "::set-env name=IS_RELEASE::$IS_RELEASE"
 # Bump up
 
 if [[ $IS_RELEASE == 'true' ]]; then
-  VERSION_PATCH=$(echo "$VERSION" | awk -F"." '{print $NF}')
+  VERSION_PATCH=$(echo "$VERSION_VALUE" | awk -F"." '{print $NF}')
   NEXT_PATCH=$(($VERSION_PATCH + 1))
-  ROOT_UNTIL=$(echo "$VERSION" | awk -F "." '{print length($0)-length($NF)}')
-  VERSION_ROOT=$(echo "$VERSION" | cut -c 1-$ROOT_UNTIL)
+  ROOT_UNTIL=$(echo "$VERSION_VALUE" | awk -F "." '{print length($0)-length($NF)}')
+  VERSION_ROOT=$(echo "$VERSION_VALUE" | cut -c 1-$ROOT_UNTIL)
   NEXT_VERSION="$VERSION_ROOT$NEXT_PATCH-SNAPSHOT"
-  sed -i -e "s/$VERSION_KEY=$VERSION/$VERSION_KEY=0.0.2-SNAPSHOT/" "$VERSION_FILE"
+  sed -i -e "s/$VERSION_KEY=$VERSION_VALUE/$VERSION_KEY=0.0.2-SNAPSHOT/" "$VERSION_FILE"
   echo "Setting next version $NEXT_VERSION"
+  echo "::set-output name=next_version::$NEXT_VERSION"
 else
   echo "Bump-up will not be executed, the version is a SNAPSHOT or has a bad format"
 fi
